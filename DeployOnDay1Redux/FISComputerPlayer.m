b05abd0024b2074062c41ae3cf7e1607d7125ab8
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-06-06 20:16:53 -0400

    Base lab complete will implement advanced methods

--- DeployOnDay1Redux/FISComputerPlayer.m
+++ DeployOnDay1Redux/FISComputerPlayer.m
@@ -8,16 +8,56 @@
 
 #import "FISComputerPlayer.h"
 
+
 @implementation FISComputerPlayer
 
 +(BOOL)isEnabled
 {
-    return NO;
+    return YES;
 }
 
 -(FISTicTacToePosition)nextPlay
 {
-    return FISTicTacToePositionMake(0, 0);
+   
+    if ([self.game canPlayAtColumn:0 row:0]) {
+        
+        return FISTicTacToePositionMake(0, 0);
+        
+    } else if ([self.game canPlayAtColumn:0 row:1]) {
+        
+        return FISTicTacToePositionMake(0, 1);
+    
+    } else if ([self.game canPlayAtColumn:0 row:2]) {
+        
+        return FISTicTacToePositionMake(0, 2);
+        
+    } else if ([self.game canPlayAtColumn:1 row:0]) {
+        
+        return FISTicTacToePositionMake(1, 0);
+        
+    } else if ([self.game canPlayAtColumn:1 row:1]) {
+        
+        return FISTicTacToePositionMake(1, 1);
+        
+    } else if ([self.game canPlayAtColumn:1 row:2]) {
+        
+        return FISTicTacToePositionMake(1, 2);
+        
+    } else if ([self.game canPlayAtColumn:2 row:0]) {
+        
+        return FISTicTacToePositionMake(2, 0);
+        
+    } else if ([self.game canPlayAtColumn:2 row:1]) {
+        
+        return FISTicTacToePositionMake(2, 1);
+        
+    } else if ([self.game canPlayAtColumn:2 row:2]) {
+        
+        return FISTicTacToePositionMake(2, 2);
+        
+    }
+
+   
 }
 
 @end
--- DeployOnDay1Redux/FISTicTacToeGame.m
+++ DeployOnDay1Redux/FISTicTacToeGame.m
@@ -17,51 +17,145 @@
 
 @implementation FISTicTacToeGame
 
--(instancetype)init
-{
+-(instancetype)init {
+
     self = [super init];
     if(self) {
         // Do initialization of your game here, inside this if statement.
         // Leave the rest of this method alone :)
-
+        
+        [self resetBoard];
+        
     }
 
     return self;
+    
 }
 
--(void)resetBoard
-{
+-(void)resetBoard {
+
+    // Draw out board. Nest 3 arrays to create 3 x 3 board
+    
+    NSArray *ticTacRow1 = @[@"", @"", @""];
+    NSArray *ticTacRow2 = @[@"", @"", @""];
+    NSArray *ticTacRow3 = @[@"", @"", @""];
+    
+    self.board = [@[[ticTacRow1 mutableCopy],
+                   [ticTacRow2 mutableCopy],
+                   [ticTacRow3 mutableCopy]]mutableCopy];
+    
+}
+
+-(NSString *)playerAtColumn:(NSUInteger)column row:(NSUInteger)row {
+    
+  //  NSLog(@"\n\n\n\n\n%lu\n\n\n\n %lu\n\n\n\n\n", column, row);
+    
+    return _board[column][row];
+    
+}
+
+-(BOOL)canPlayAtColumn:(NSUInteger)column row:(NSUInteger)row {
+    
+    if ([_board[column][row] isEqualToString:@""]) {
+        
+        return YES;
+        
+    } else {
+        
+        return NO;
+        
+    }
 
 }
 
--(NSString *)playerAtColumn:(NSUInteger)column row:(NSUInteger)row
-{
+-(void)playXAtColumn:(NSUInteger)column row:(NSUInteger)row {
+
+    BOOL canPlay = [self canPlayAtColumn:column row:row];
+    
+ //   NSUInteger location = [_board[column][row] unsignedIntegerValue];
+    
+    if (canPlay == YES) {
+
+        _board[column][row] = @"X";
+        
+    }
+    
+}
+
+-(void)playOAtColumn:(NSUInteger)column row:(NSUInteger)row {
+
+    BOOL canPlay = [self canPlayAtColumn:column row:row];
+    
+    if (canPlay == YES) {
+        
+        _board[column][row] = @"O";
+        
+    }
+    
+}
+
+-(NSString *)winningPlayer {
+    
+    // Horizontal Wins
+    if ([_board[0][0] isEqualToString:_board[0][1]] && [_board[0][0] isEqualToString:_board[0][2]]  ) {
+        
+        return _board[0][0];
+        
+    } else if ([_board[1][0] isEqualToString:_board[1][1]] && [_board[1][0] isEqualToString:_board[1][2]] ) {
+        
+        return _board[1][0];
+        
+    } else if ([_board[2][0] isEqualToString:_board[2][1]] && [_board[2][0] isEqualToString:_board[2][2]]) {
+        
+        return _board[2][0];
+        
+    
+    } //Vertical Wins
+    else if ([_board[0][0] isEqualToString:_board[1][0]] && [_board[0][0] isEqualToString:_board[2][0]]) {
+        
+        return _board[0][0];
+        
+    } else if ([_board[0][1] isEqualToString:_board[1][1]] && [_board[0][1] isEqualToString:_board[2][1]]) {
+        
+        return _board[0][1];
+        
+    } else if ([_board[0][2] isEqualToString:_board[1][2]] && [_board[0][2] isEqualToString:_board[2][2]]) {
+        
+        return _board[0][2];
+        
+    } // Diagonal wins
+    else if ([_board[0][0] isEqualToString:_board[1][1]] && [_board[0][0] isEqualToString:_board[2][2]]) {
+        
+        return _board[0][0];
+        
+    } else if ([_board[2][0] isEqualToString:_board[1][1]] && [_board[2][0] isEqualToString:_board[0][2]]) {
+        
+        return _board[2][0];
+        
+    } else {
+    
     return @"";
+        
+    }
+
 }
 
--(BOOL)canPlayAtColumn:(NSUInteger)column row:(NSUInteger)row
-{
+-(BOOL)isADraw {
+
+    for (NSUInteger c = 0; c < [_board count]; c++) {
+        for (NSUInteger r = 0; r < [_board count]; r++) {
+            
+            if ([_board[c][r] isEqualToString:@""]) {
+                
+                return NO;
+            }
+            
+        }
+    
+    }
+    
     return YES;
-}
-
--(void)playXAtColumn:(NSUInteger)column row:(NSUInteger)row
-{
-
-}
-
--(void)playOAtColumn:(NSUInteger)column row:(NSUInteger)row
-{
-
-}
-
--(NSString *)winningPlayer
-{
-    return @"";
-}
-
--(BOOL)isADraw
-{
-    return NO;
+    
 }
 
 @end
